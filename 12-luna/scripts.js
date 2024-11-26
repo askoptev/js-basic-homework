d1f3af2a2f407f65394f29894e9ff8a13abfdc24
Alexander Koptev <askoptevwork@gmail.com>
2024-11-26 21:29:08 +0300

    12.11. Домашнее задание - Работа со строками

---
+++ 12-luna/scripts.js
@@ -0,0 +1,30 @@
+let card = "4561-1213-4367-2612";
+
+const isValidCard = (card) => {
+  if (typeof card !== "string") {
+    return false;
+  }
+  let str = card.replaceAll("-", "");
+  if (isNaN(Number(str))) {
+    return false;
+  }
+  let arr = Array.from(str, (item) => Number(item));
+  let lunaArray = arr.map((item, index) => {
+    if ((index + 1) % 2 != 0) {
+      item = item * 2;
+      if (item > 9) {
+        item = item - 9;
+      }
+    }
+    return item;
+  });
+  
+  let sum = lunaArray.reduce((acc, item) => acc + item);
+  let result = sum % 10 == 0 ?? false;
+
+  return result;
+};
+
+console.log(`Карта: ${card} - ${isValidCard(card) ? "" : "не"} корректна`);
+
+
