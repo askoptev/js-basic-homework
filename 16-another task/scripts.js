d0b92340f902396371f9971641699e1ac7ded120
askoptev <askoptevwork@gmail.com>
2025-01-30 13:40:57 +0300

    fix codereview

--- 16-another task/scripts.js
+++ 16-another task/scripts.js
@@ -1,24 +1,22 @@
 const ToDoList = {
   tasks: [],
+  lastId: 0,
   addTask(order = 0, name = "") {
     order = order < 0 ? 0 : order > 5 ? 5 : order;
+    const id = ++this.lastId;
     this.tasks.push({
-      id: this.tasks.length + 1,
-      order: order,
-      name: name,
+      id,
+      order,
+      name,
     });
     return this.tasks.at(-1);
-  },
-  deleteTask(id) {
-    this.tasks = this.tasks.filter((item) => item.id !== id);
-    return this.tasks;
   },
   updateTask(id, name, order) {
     const task = this.tasks.find((f) => f.id === id);
     if (!task) {
       console.log("Задача не найдена");
       return;
-    }    
+    }
     this.tasks = this.tasks.map((item) => {
       if (item.id === id) {
         item.name = name ? name : item.name;
@@ -28,6 +26,10 @@ const ToDoList = {
       return item;
     });
     return this.tasks.filter((item) => item.id === id);
+  },
+  deleteTask(id) {
+    this.tasks = this.tasks.filter((item) => item.id !== id);
+    return this.tasks;
   },
   sortTasks(id, order) {
     switch (true) {
@@ -43,48 +45,39 @@ const ToDoList = {
   },
 };
 
-
-const newTasks = {
+const newTask = {
   tasks: [],
   lastId: 0,
-  addTask(order = 0, name = "", description = "") {
-    order = order < 0 ? 0 : order > 5 ? 5 : order;
-    const id = ++this.lastId;
-    this.tasks.push({
-      id,
-      order,
-      name,
-      description,
-    });
-    return this.tasks.at(-1);
-  },
-  updateTask(id, name, order, description) {
-    const task = this.tasks.find((f) => f.id === id);
-    if (!task) {
-      console.log("Задача не найдена");
-      return;
+}
+
+newTask.addTask = function(order = 0, name = "", description="") {
+  const task = ToDoList.addTask.call(this, order, name);
+  this.tasks.map(item => {
+    if (item.id === task.id) {
+      item.description = description;
     }
-    this.tasks = this.tasks.map((item) => {
-      if (item.id === id) {
-        item.name = name ? name : item.name;
-        item.description = description ? description : item.description;
-        order = order < 0 ? 0 : order > 5 ? 5 : order;
-        item.order = order ? order : item.order;
-      }
-      return item;
-    });
-    return this.tasks.filter((item) => item.id === id);
-  },
+  })  
+  return this.tasks.at(-1);
 };
 
+newTask.updateTask = function(id, order, name, description) {
+  ToDoList.updateTask.call(this, id, order, name);
+  const task = this.tasks.find((f) => f.id === id);
+  if (!task) {
+    console.log("Задача не найдена");
+    return;
+  }
+  this.tasks.map((item) => {
+    if (item.id === id) {
+      item.description = description ? description : item.description;
+    }
+  });
+  return this.tasks.filter((item) => item.id === id);
+};
 
-
-newTasks.deleteTask = ToDoList.deleteTask;
-newTasks.sortTasks = ToDoList.sortTasks;
-
-console.log(newTasks.addTask(order=6, name="newOne", description="newDesc"));
-console.log(newTasks.addTask(order=2, name="newTwo"));
-console.log(newTasks.addTask(order=2, name="newThree"));
-console.log(newTasks.deleteTask((id = 1)));
-console.log(newTasks.updateTask(id=2, name="updateTwo", order=1, description='update'));
-console.log(newTasks.sortTasks(id = 0, order=-1));
+console.log(newTask.addTask(order=6, name="newName-1", description="newDesc-1"));
+console.log(newTask.addTask(order=6, name="newName-2", description="newDesc-2"));
+console.log(newTask.addTask(order=6, name="newName-3", description="newDesc-3"));
+console.log(newTask.updateTask(id=2, name="updateName-2", order=1, description='updateDesc-2'));
+console.log(ToDoList.deleteTask.call(newTask, id = 1));
+console.log(ToDoList.sortTasks.call(newTask, id = 0, order=-1));
